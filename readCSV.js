511a82b6851b51e99539c48cde800bffe1e9bbc4
Clayton-McCormack <132096308+Clayton-McCormack@users.noreply.github.com>
2024-01-12 20:31:13 -0500

    Making it so the info from the CSV is loaded automatically.

--- readCSV.js
+++ readCSV.js
@@ -1,22 +1,24 @@
 $(document).ready(function () {
-    $('#csvFileInput').on('change', function (e) {
-        var file = e.target.files[0];
-        var reader = new FileReader();
-        
-        reader.onload = function (e) {
-            var csvData = e.target.result;
-            var data = $.csv.toArrays(csvData);
-            displayCSVData(data);
-        };
-
-        reader.readAsText(file);
+    $.ajax({
+        type: "GET",
+        url: "./MOCK_DATA.csv",
+        dataType: "text",
+        success: function(data) {
+            var csvData = $.csv.toArrays(data);
+            displayCSVData(csvData);
+        }
     });
 
     function displayCSVData(data) {
-        var html = '<table border="1">';
-        data.forEach(function (row) {
+        var html = '<table><tr>';
+        // Assuming first row of csvData contains headers
+        data[0].forEach(function(header) {
+            html += '<th>' + header + '</th>';
+        });
+        html += '</tr>';
+        data.slice(1).forEach(function(row) {
             html += '<tr>';
-            row.forEach(function (cell) {
+            row.forEach(function(cell) {
                 html += '<td>' + cell + '</td>';
             });
             html += '</tr>';
